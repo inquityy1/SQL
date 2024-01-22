054f5b82d3594f03b84587c904b13dc044084e37
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-01-22 19:27:02 +0100

    Add delete possibility

--- api/social-repo/src/repos/user-repo.js
+++ api/social-repo/src/repos/user-repo.js
@@ -34,7 +34,14 @@ class UserRepo {
     return toCamelCase(rows)[0];
   }
 
-  static async delete(id) {}
+  static async delete(id) {
+    const { rows } = await pool.query(
+      'DELETE FROM users WHERE id = $1 RETURNING *;',
+      [id]
+    );
+
+    return toCamelCase(rows)[0];
+  }
 }
 
 module.exports = UserRepo;
