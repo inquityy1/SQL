cfb682d4c3753fa4b6ef35abdd7834451b956d2e
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-01-23 18:17:07 +0100

    Add tests and troubleshooting

---
+++ api/social-repo/src/test/routes/users.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const buildApp = require('../../app');
+const UserRepo = require('../../repos/user-repo');
+const pool = require('../../pool');
+
+beforeAll(() => {
+  return pool.connect({
+    host: 'localhost',
+    port: 5432,
+    database: 'socialnetwork',
+    user: 'postgres',
+    password: 'qqwwee11',
+  });
+});
+
+it('create a user', async () => {
+  const startingCount = await UserRepo.count();
+  expect(startingCount).toEqual(0);
+
+  await request(buildApp())
+    .post('/users')
+    .send({ username: 'testuser', bio: 'test bio' })
+    .expect(200);
+
+  const finishCount = await UserRepo.count();
+  expect(finishCount).toEqual(1);
+});
