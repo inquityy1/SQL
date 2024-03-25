b201cc60c352fde835e7ca1ff5cc8c3e3ae8aa1d
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-03-25 16:54:22 +0100

    Add insert and select query for our table

---
+++ sql-complete-guide/03-insert-users-data.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (yearly_salary, full_name, current_status) 
+-- VALUES (100000, 'Nebojsa Nikolic', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (100000, 'Nemanja Nikolic', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (100000, 'Kim Kardashian', 'unemployed');
