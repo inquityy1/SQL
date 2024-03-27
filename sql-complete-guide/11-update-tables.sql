f207f86b770ef357c560de9e76854855a0312660
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-03-27 13:22:15 +0100

    Update (ALTER) table

---
+++ sql-complete-guide/11-update-tables.sql
@@ -0,0 +1,8 @@
+ALTER TABLE employers
+ALTER COLUMN yearly_revenue SET DATA TYPE FLOAT; -- Postgresql
+
+-- ALTER TABLE employers
+-- MODIFY COLUMN yearly_revenue FLOAT(5,2); -- MySQL
+
+ALTER TABLE users
+ALTER COLUMN full_name SET DATA TYPE VARCHAR(300);
