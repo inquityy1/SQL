80a03350b433b476b836fd120a386c7ec6f0af26
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-15 15:20:46 +0200

    Combinational comparisons

--- crud-operations/08-basic-filtering.sql
+++ crud-operations/08-basic-filtering.sql
@@ -1,5 +1,8 @@
 -- SELECT * FROM sales
 -- WHERE volume > 1000;
 
+-- SELECT * FROM sales
+-- WHERE is_recurring IS TRUE;
+
 SELECT * FROM sales
-WHERE is_recurring IS TRUE;
+WHERE (is_disputed IS TRUE) AND (volume > 5000);
