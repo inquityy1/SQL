6bd9305f9b91e3021f31451b86360cfc115e7b0e
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-16 19:27:00 +0200

    RIGHT JOIN and filtering

--- relations/06-left-join.sql
+++ relations/06-left-join.sql
@@ -1,4 +1,5 @@
 SELECT *
-FROM addresses AS a
-LEFT JOIN users AS u ON a.id = u.address_id
-LEFT JOIN cities AS c ON c.id = a.city_id;
+FROM users AS u
+RIGHT JOIN addresses AS a ON a.id = u.address_id
+LEFT JOIN cities AS c ON c.id = a.city_id
+ORDER BY u.id;
