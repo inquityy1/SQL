5cf627a04699ca917c4a898b5684e41cbe9a47fb
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-17 21:03:03 +0200

    Adding tables and relations

---
+++ relations/18-insert-company-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, birthdate, email)
+VALUES ('Nebojsa', 'Nikolic', '2001-03-08', 'nebojsa@test.com');
+
+INSERT INTO intranet_accounts (email, password)
+VALUES ('nebojsa@test.com', 'qqwwee11');
