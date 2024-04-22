7fff74f2de7d2ef03a9c9737a513f523bab20ba7
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-22 19:26:41 +0200

    JOIN and GROUP BY together

--- 7.aggregate-functions/10-group-by.sql
+++ 7.aggregate-functions/10-group-by.sql
@@ -2,5 +2,12 @@
 -- FROM bookings
 -- GROUP BY booking_date;
 
-SELECT DISTINCT booking_date, SUM(num_guests)
-FROM bookings;
+-- SELECT p.name, SUM(b.num_guests)
+-- FROM payment_methods AS p
+-- INNER JOIN bookings AS b ON p.id = b.payment_id
+-- GROUP BY p.name;
+
+SELECT p.name, b.booking_date, SUM(b.num_guests), ROUND(AVG(amount_tipped), 2)
+FROM payment_methods AS p
+INNER JOIN bookings AS b ON p.id = b.payment_id
+GROUP BY p.name, b.booking_date;
