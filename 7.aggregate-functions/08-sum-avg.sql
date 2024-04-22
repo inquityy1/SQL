2e95a895f371c5f9737ea9d989a1e676caff9f7c
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-22 17:55:21 +0200

    SUM and AVG functions

---
+++ 7.aggregate-functions/08-sum-avg.sql
@@ -0,0 +1,4 @@
+-- SELECT SUM(amount_billed) FROM bookings;
+-- SELECT AVG(num_guests) FROM bookings;
+-- SELECT ROUND(AVG(num_guests)) FROM bookings;
+SELECT ROUND(AVG(amount_tipped), 4) FROM bookings;
