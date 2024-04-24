ebd83786113e486e325b5ee767690b65f6825031
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-04-24 19:11:03 +0200

    Working with weekdays

--- 8.mathematical-functions/07-date-time-functions.sql
+++ 8.mathematical-functions/07-date-time-functions.sql
@@ -1,2 +1,6 @@
-SELECT EXTRACT(MONTH FROM last_checkin), last_checkin
+-- DOW postgreSQL only
+-- SELECT EXTRACT(DOW FROM last_checkin), last_checkin
+-- FROM memberships;
+
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
 FROM memberships;
