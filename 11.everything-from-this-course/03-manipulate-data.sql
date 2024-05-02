1f69d51de552137ec268d7dbcb95bb2cfd99d974
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-02 20:46:48 +0200

    UPDATE, DELETE, SELECT

--- 11.everything-from-this-course/03-manipulate-data.sql
+++ 11.everything-from-this-course/03-manipulate-data.sql
@@ -1,22 +1,29 @@
-INSERT INTO events (
-    name,
-    date_planned,
-    description,
-    max_participants,
-    min_age
-)
-VALUES 
-    (
-        'My birthay',
-        '2024-03-08 16:30:00',
-        'Birthay party for Nebojsa',
-        20,
-        18
-    ),
-    (
-        'A second Event',
-        '2024-06-18 16:30:00',
-        'This is a second event',
-        21,
-        16
-    );
+-- INSERT INTO events (
+--     name,
+--     date_planned,
+--     description,
+--     max_participants,
+--     min_age
+-- )
+-- VALUES 
+--     (
+--         'My birthay',
+--         '2024-03-08 16:30:00',
+--         'Birthay party for Nebojsa',
+--         20,
+--         18
+--     ),
+--     (
+--         'A second Event',
+--         '2024-06-18 16:30:00',
+--         'This is a second event',
+--         21,
+--         16
+--     );
+
+UPDATE events
+SET min_age = 21
+WHERE id = 1;
+
+-- DELETE FROM events
+-- WHERE id = 1;
