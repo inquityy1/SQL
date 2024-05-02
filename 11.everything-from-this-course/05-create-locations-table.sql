2854174899af2c8d5a3651b0fae26ad6a4b0dfdb
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-02 21:35:46 +0200

    Relations and foreign keys

--- 11.everything-from-this-course/05-create-locations-table.sql
+++ 11.everything-from-this-course/05-create-locations-table.sql
@@ -1,11 +1,23 @@
+CREATE TABLE cities (
+    name VARCHAR(200) PRIMARY KEY,
+);
+
 CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     title VARCHAR(300),
     street VARCHAR(300) NOT NULL,
     house_number VARCHAR(10) NOT NULL,
     postal_code VARCHAR(5) NOT NULL,
+    city_name VARCHAR(200) REFERENCES cities (name)
 );
 
-CREATE TABLE cities (
-    name VARCHAR(200) PRIMARY KEY,
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(300) NOT NULL CHECK (LENGTH(name) > 5),
+    date_planned TIMESTAMP NOT NULL,
+    image VARCHAR(300),
+    description TEXT NOT NULL,
+    max_participants INT CHECK (max_participants > 0),
+    min_age INT CHECK (min_age > 0),
+    location_id INT REFERENCES locations (id)
 );
