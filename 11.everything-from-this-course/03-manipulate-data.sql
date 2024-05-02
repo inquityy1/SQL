0003d93b661ec89f3e1da8efc5ce036466d34237
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-02 20:37:19 +0200

    Inserting data

---
+++ 11.everything-from-this-course/03-manipulate-data.sql
@@ -0,0 +1,22 @@
+INSERT INTO events (
+    name,
+    date_planned,
+    description,
+    max_participants,
+    min_age
+)
+VALUES 
+    (
+        'My birthay',
+        '2024-03-08 16:30:00',
+        'Birthay party for Nebojsa',
+        20,
+        18
+    ),
+    (
+        'A second Event',
+        '2024-06-18 16:30:00',
+        'This is a second event',
+        21,
+        16
+    );
