18acf19e226fc63210efaff8464b210198d1b926
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-03 21:03:52 +0200

    Multiple joins

--- 11.everything-from-this-course/10-queries-with-relations.sql
+++ 11.everything-from-this-course/10-queries-with-relations.sql
@@ -1,10 +1,23 @@
+-- SELECT 
+--     e.id AS event_id,
+--     e.name,
+--     e.date_planned, 
+--     loc.title, 
+--     loc.street, 
+--     loc.house_number, 
+--     loc.city_name 
+-- FROM events AS e
+-- INNER JOIN locations AS loc ON e.location_id = loc.id;
+
 SELECT 
     e.id AS event_id,
     e.name,
-    e.date_planned, 
-    loc.title, 
-    loc.street, 
-    loc.house_number, 
-    loc.city_name 
+    e.date_planned,
+    loc.title,
+    loc.street,
+    loc.house_number,
+    loc.city_name,
 FROM events AS e
-INNER JOIN locations AS loc ON e.location_id = loc.id;
+INNER JOIN locations AS loc ON e.location_id = loc.id
+INNER JOIN events_users AS eu ON eu.event_id = e.id
+INNER JOIN users AS u ON eu.user_id = u.id;
