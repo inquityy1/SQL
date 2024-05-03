3792140eeef8a1b1455bcc719e94be4214597997
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-03 21:41:33 +0200

    Filtering with HAVING

--- 11.everything-from-this-course/11-more-queries.sql
+++ 11.everything-from-this-course/11-more-queries.sql
@@ -3,6 +3,12 @@
 
 -- SELECT COUNT(id) FROM locations;
 
+-- SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
+-- LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- GROUP BY c.name;
+
 SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
 LEFT JOIN locations AS loc ON loc.city_name = c.name
-GROUP BY c.name;
+-- WHERE loc.city_name = 'Munich'
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1;
